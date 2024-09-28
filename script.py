93c1efb82d06c066f10fa260bf738986da5f61d4
MagisterLatka <michallatka3@wp.pl>
2024-09-28 23:48:17 +0200

    Weighted A star

--- script.py
+++ script.py
@@ -1,13 +1,40 @@
 import networkx as nx
 import osmnx as ox
+import os.path
 
-print(ox.__version__)
+def getLocation(name):
+    y,x = ox.geocoder.geocode(name)
+    return x, y
 
-G = ox.graph_from_place("Rogoźno, 64-610, Poland", network_type="bike")
+def getOrLoadMap(name):
+    if os.path.isfile("maps/" + name):
+        return ox.load_graphml("maps/" + name)
+    G = ox.graph_from_place(name, network_type="bike")
+    ox.save_graphml(G, "maps/" + name)
+    return G
+
+def lengthBetweenNodes(fromNode, toNode):
+    node1 = G.nodes[fromNode]
+    x1 = node1['x']
+    y1 = node1['y']
+    node2 = G.nodes[toNode]
+    x2 = node2['x']
+    y2 = node2['y']
+    return ox.distance.great_circle(y1, x1, y2, x2)
+
+def weightFun(fromNode, toNode, dicts):
+    banned = ['motorway', 'trunk', 'primary', 'motorway_link', 'trunk_link', 'bus_guideway', 'busway']
+    return None if dicts.get('highway', '') in banned and dicts.get('length', lengthBetweenNodes(fromNode, toNode)) > 100 else dicts.get('length', lengthBetweenNodes(fromNode, toNode))
+
+G = getOrLoadMap("Kraków, Poland")#str(input("Pass map: ")))
 #fig, ax = ox.plot_graph(G, node_color="r")
 
-nodeA = ox.distance.nearest_nodes(G, 16.99249, 52.75207)
-nodeB = ox.distance.nearest_nodes(G, 16.97167, 52.75430)
-nodes = nx.astar_path(G, nodeA, nodeB, weight="length")
-ox.plot_graph_route(G, nodes, 'r')
+while True:
+    print("-----------")
+    nodeA = ox.distance.nearest_nodes(G, *getLocation("TAURON Arena Kraków"))#input("Pass first loc: ")))
+    nodeB = ox.distance.nearest_nodes(G, *getLocation("M1 Kraków"))#input("Pass second loc: ")))
+    nodes = nx.astar_path(G, nodeA, nodeB, weight=weightFun)
+#    for node in nodes:
+#        print(G[node])
+    ox.plot_graph_route(G, nodes, 'r')
 
